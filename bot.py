a7db565c69a88a1c8e382182756247c2c8f5c326
Gary05CX <86848488+Gary05CX@users.noreply.github.com>
2021-07-03 19:11:55 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='?',intents=intents)
+
+@bot.event
+async def on_ready():
+    print(">> a1itt1ebot is fucking online!!! <<")
+
+@bot.event
+async def on_member_join(member):
+    print(f"{member} join!")
+    channel = bot.get_channel(710844919954276422)
+    await channel.send(f"{member} join!")
+
+@bot.event
+async def on_member_remove(member):
+    print(f"{member} leave!")
+    channel = bot.get_channel(710844919954276422)
+    await channel.send(f"{member} leave!")
+
+bot.run('ODYwNTU0NTY0NTA3OTI2NTg4.YN875A.6ibKkKNESjNNl9cy-EA3nUo1t6A')
