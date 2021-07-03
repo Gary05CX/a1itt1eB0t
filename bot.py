afe955d1a650fd666d61e8025536fa561c914362
Gary05CX <86848488+Gary05CX@users.noreply.github.com>
2021-07-03 22:38:14 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ bot = commands.Bot(command_prefix='?',intents=intents)
 async def on_ready():
     print(">> a1itt1ebot is online!!! <<")
     channel = bot.get_channel(860843545387859979)
-    await channel.send("a1itt1eB1t is online")
+    await channel.send("a1itt1eB1t is online!!")
 
 @bot.event
 async def on_member_join(member):
@@ -22,5 +22,14 @@ async def on_member_remove(member):
     print(f"{member} leave!")
     channel = bot.get_channel(860843545387859979)
     await channel.send(f"{member} leave!")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"{round(bot.latency*1000)} ms")
+    print(bot.latency)
+
+@bot.command()
+async def patgay(ctx):
+    await ctx.send("(@patfat#4559) is GAY")
 
 bot.run('ODYwNTU0NTY0NTA3OTI2NTg4.YN875A.6ibKkKNESjNNl9cy-EA3nUo1t6A')
