ada92f7504763c18f9548e3400dea1fe0769ffcb
Gary05CX <86848488+Gary05CX@users.noreply.github.com>
2021-07-04 02:45:31 +0800

    Create a1itt1eb0t.py

---
+++ a1itt1eb0t.py
@@ -0,0 +1,62 @@
+import discord
+from discord.ext import commands
+import json
+import random
+
+with open('setting.json','r',encoding='utf8') as jfile:
+        jdata = json.load(jfile)
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='?',intents=intents)
+
+@bot.event
+async def on_ready():
+    print(">> a1itt1ebot is online!!! <<")
+    channel = bot.get_channel(int(jdata['A1itt1eB0t']))
+    await channel.send("a1itt1eB0t is online!!")
+
+@bot.event
+async def on_member_join(member):
+    print(f"{member} join!")
+    channel = bot.get_channel(int(jdata['A1itt1eB0t']))
+    await channel.send(f"{member} join!")
+
+@bot.event
+async def on_member_remove(member):
+    print(f"{member} leave!")
+    channel = bot.get_channel(int(jdata['A1itt1eB0t']))
+    await channel.send(f"{member} leave!")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"{round(bot.latency*1000)} ms")
+    print(bot.latency)
+
+@bot.command()
+async def patgay(ctx):
+    await ctx.send("(@patfat#4559) is GAY")
+
+@bot.command()
+async def morningstar(ctx):
+    await ctx.send("Sam is Gay")
+
+@bot.command()
+async def sampic(ctx):
+    random_pic = random.choice(jdata['sam'])
+    pic = discord.File(random_pic)
+    await ctx.send(file= pic)
+
+@bot.command()
+async def patpic(ctx):
+    random_pic = random.choice(jdata['patgay'])
+    pic = discord.File(random_pic)
+    await ctx.send(file= pic)
+    
+@bot.command()
+async def gary(ctx):
+    await ctx.send("[2021-07-04-->1015:go out;(maybe)1500:back home and free];\
+        [2021-07-05-->0800:go out;(maybe)1500:back home and free];\
+            [2021-07-06-->0800:go out;(maybe)1500:back home and free]")
+
+bot.run(jdata['TOKEN'])
