911c71d06f36c169c45a36af26ae158f350901eb
Gary05CX <86848488+Gary05CX@users.noreply.github.com>
2021-07-05 23:35:52 +0800

    ああああああああ

--- cmds/onmsg.py
+++ cmds/onmsg.py
@@ -6,7 +6,18 @@ class Onmsg(Cog_Extension):
     @commands.Cog.listener()
     async def on_message(self, msg):
         if msg.content == 'ばか' and msg.author.id == 626419302232227841 or msg.author.id == 723473163341791314 :
-            await msg.channel.send('は？')        
+            await msg.channel.send('は？')
+
+    @commands.Cog.listener()
+    async def on_message(self, msg):
+        if msg.content.endswith('fuck') :
+            await msg.channel.send('yourself')        
+ 
+    @commands.Cog.listener()
+    async def on_message(self, msg):
+        if msg.content == 'あくあ':
+            await msg.channel.send(' <@!626419302232227841> 的老婆 ')
+
 
 def setup(bot):
     bot.add_cog(Onmsg(bot))
