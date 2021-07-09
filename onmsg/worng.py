368ad6b6b518039768d6339cb0bebf362a4e7570
Gary05CX <86848488+Gary05CX@users.noreply.github.com>
2021-07-09 21:53:18 +0800

    1

--- onmsg/worng.py
+++ onmsg/worng.py
@@ -7,9 +7,11 @@ class wrong(Cog_Extension):
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.errors.MissingRequiredArgument):
-            await ctx.send("MissingRequiredArgument!!")
+            await ctx.send("Missing required argument 遺失參數!!")
         elif isinstance(error, commands.errors.CommandNotFound):
              await ctx.send("Commands not found  沒這指令!!")
+        else:
+            await ctx.send("something goes wrong  發生錯誤!!!")
 
 def setup(bot):
     bot.add_cog(wrong(bot))
