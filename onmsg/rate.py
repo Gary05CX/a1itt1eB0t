132ef84e25d1a32749e94c7745a8243ed5c12bb9
Gary05CX <86848488+Gary05CX@users.noreply.github.com>
2021-07-19 19:31:37 +0800

    etar

--- onmsg/rate.py
+++ onmsg/rate.py
@@ -7,12 +7,40 @@ class rate(Cog_Extension):
 
     @commands.Cog.listener()
     async def on_message(self, msg):
-        if msg.content.endswith("rate") or msg.content.endswith("機率"):
-            a = randrange(100)
-            if a > 50:
-                await msg.channel.send("是"+","+str(a)+"%")
-            elif a < 50:
-                await msg.channel.send("否"+","+str(a)+"%")
-
+        RATE = ["rate","機率"]
+        a = randrange(100)
+        b = randrange(100)
+        if msg.content in RATE:
+            possive = ["a"]
+            negative = ["gay", "ばか", "白痴", "on9", "6uo"]
+            if msg.content in negative:
+                passid = [626419302232227841]
+                if msg.author.id not in passid:
+                    if a >= b:
+                        if a >= 50:
+                            await msg.channel.send("YES," + str(a) + "%")
+                        elif a < 50:
+                            await msg.channel.send("NO," + str(a) + "%")
+                    if b > a:
+                        if b >= 50:
+                            await msg.channel.send("YES," + str(b) + "%")
+                        elif b < 50:
+                            await msg.channel.send("NO," + str(b) + "%")
+                elif msg.author.id in passid:
+                    if a <= b:
+                        if a >= 50:
+                            await msg.channel.send("YES," + str(a) + "%")
+                        elif a < 50:
+                            await msg.channel.send("NO," + str(a) + "%")
+                    if b < a:
+                        if b >= 50:
+                            await msg.channel.send("YES," + str(b) + "%")
+                        elif b < 50:
+                            await msg.channel.send("NO," + str(b) + "%")
+            else:
+                if b >= 50:
+                    await msg.channel.send("YES," + str(b) + "%")
+                elif b < 50:
+                    await msg.channel.send("NO," + str(b) + "%")
 def setup(bot):
     bot.add_cog(rate(bot))
