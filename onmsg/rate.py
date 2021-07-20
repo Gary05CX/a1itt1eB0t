63fc9fbc3143d25cddd6f91344da2a0978968733
Gary05CX <86848488+Gary05CX@users.noreply.github.com>
2021-07-20 22:46:05 +0800

    1

--- onmsg/rate.py
+++ onmsg/rate.py
@@ -8,8 +8,8 @@ class rate(Cog_Extension):
     @commands.Cog.listener()
     async def on_message(self, msg):
         RATE = ("rate","機率")
-        a = randrange(101) 
-        b = randrange(101)
+        a = randrange(100) + 1
+        b = randrange(100) + 1
         c = a+b
         list = [a, b, c]
         if msg.content.endswith(RATE) :
